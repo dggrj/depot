2a71ace87c74a6ddac5ed93eba8c0be8f13fec8f
Jay Cliffe <dggrj@hotmail.com>
2011-12-09 16:25:47 -0800

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {minimum: 10, message: "seems a bit short, don't you think?"}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)$}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
