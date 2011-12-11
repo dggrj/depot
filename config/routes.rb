d5a9437b63add369415bc72f5903ddcc4f623f05
Jay Cliffe <dggrj@hotmail.com>
2011-12-11 11:07:47 -0800

    Store front

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 Depot::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
   # The priority is based upon order of creation:
@@ -59,6 +61,7 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'store#index', as: 'store'
 
   # See how all your routes lay out with "rake routes"
 
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -13,21 +13,19 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new( title: "My Title",
-                           description: "desc",
-                           image_url: "img.jpg" )
-    assert product.invalid?
-
+    product = Product.new(title:       "My Book Title",
+                          description: "yyy",
+                          image_url:   "zzz.jpg")
     product.price = -1
     assert product.invalid?
-    assert_equal "must be greater than or equal to 0.01",
+    assert_equal "must be greater than or equal to 0.01", 
       product.errors[:price].join('; ')
-      
+
     product.price = 0
     assert product.invalid?
-    assert_equal "must be greater than or equal to 0.01",
+    assert_equal "must be greater than or equal to 0.01", 
       product.errors[:price].join('; ')
-      
+
     product.price = 1
     assert product.valid?
   end
